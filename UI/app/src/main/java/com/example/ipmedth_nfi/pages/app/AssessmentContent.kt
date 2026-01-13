73ac0ac5bbb7cf885ed9151d28e29afc4ec1b6a1
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2026-01-13 15:21:56 +0100

    fix(): Fixed AndroidManifest.xml and a lot of other stuff

--- UI/app/src/main/java/com/example/ipmedth_nfi/pages/app/AssessmentContent.kt
+++ UI/app/src/main/java/com/example/ipmedth_nfi/pages/app/AssessmentContent.kt
@@ -1,10 +1,13 @@
 package com.example.ipmedth_nfi.pages.app
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import com.example.ipmedth_nfi.pages.app.tabs.*
 import com.example.ipmedth_nfi.ui.navigation.AssessmentPage
 import com.example.ipmedth_nfi.viewmodel.SessionViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AssessmentContent(
     page: AssessmentPage,
--- UI/app/src/main/java/com/example/ipmedth_nfi/pages/app/tabs/ThemesTab.kt
+++ UI/app/src/main/java/com/example/ipmedth_nfi/pages/app/tabs/ThemesTab.kt
@@ -1,16 +1,28 @@
 package com.example.ipmedth_nfi.pages.app.tabs
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.ipmedth_nfi.ui.components.InfoSubheader
 import com.example.ipmedth_nfi.viewmodel.SessionViewModel
 
 @Composable
 fun ThemesTab(viewModel: SessionViewModel) {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Themes Tab")
+    var showAddDialog by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.fillMaxSize()) {
+        InfoSubheader(
+            "Hoofdthema's",
+            "X thema's",
+            "+ Thema",
+            onAddClick = { showAddDialog = true }
+        )
     }
 }
