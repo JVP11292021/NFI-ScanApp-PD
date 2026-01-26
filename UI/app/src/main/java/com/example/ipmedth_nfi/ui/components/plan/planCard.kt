265f4a88a9b4d50fdd7acfba82f76a47b43a3845
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2026-01-26 11:15:36 +0100

    fix(): Refactored detail page & functionality

---
+++ UI/app/src/main/java/com/example/ipmedth_nfi/ui/components/plan/planCard.kt
@@ -0,0 +1,17 @@
+package com.example.ipmedth_nfi.ui.components.plan
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun planCard(
+    modifier: Modifier = Modifier,
+
+) {
+    Card(
+
+    ) {
+
+    }
+}
